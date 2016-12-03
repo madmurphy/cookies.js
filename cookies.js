5db7308d253100426179268b96bce5d58ec92c1e
Philip Ebels <philip@22websites.com>
2016-12-03 10:36:51 +0100

    Export framework as a module

--- cookies.js
+++ cookies.js
@@ -23,7 +23,7 @@
 |*|
 \*/
 
-var docCookies = {
+module.exports = {
 	getItem: function (sKey) {
 		if (!sKey) { return null; }
 		return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
