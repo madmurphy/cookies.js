c2eba5d75dabb13f79be4de962e0a86277b30748
DXet <hoenddhime@gmail.com>
2017-04-20 04:29:38 +0300

    add d.ts for typescript projects (#4)
    @DXet Done

---
+++ cookiesjs.d.ts
@@ -0,0 +1,20 @@
+interface IDocCookies {
+    getItem(sKey: string): string;
+
+    setItem(
+        sKey: string,
+        sValue: string,
+        vEnd?: number | string | Date,
+        sPath?: string,
+        sDomain?: string,
+        bSecure?: string
+    ): boolean;
+
+    removeItem(sKey: string, sPath?: string, sDomain?: string): boolean;
+
+    hasItem(sKey: string): boolean;
+
+    keys(): string[];
+}
+
+declare var docCookies: IDocCookies;
